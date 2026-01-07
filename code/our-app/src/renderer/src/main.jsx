28ed663be140592daf7dfd89b99fd991757b539c
Developer <23010584@st.phenikaa-uni.edu.vn>
2026-01-07 23:56:50 +0700

    Thêm React entry point

--- code/our-app/src/renderer/src/main.jsx
+++ code/our-app/src/renderer/src/main.jsx
@@ -1,11 +1,10 @@
-import './assets/main.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/globals.css';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
