56d70915ad8d1a2069072d82214fc11b4f581d1a
Developer <23010584@st.phenikaa-uni.edu.vn>
2026-01-12 00:14:16 +0700

    fix main.jsx

--- code/our-app/src/renderer/src/main.jsx
+++ code/our-app/src/renderer/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/globals.css';
+import './styles/globals.css';  // ← Sửa lại dòng này
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
